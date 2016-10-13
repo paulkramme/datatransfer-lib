d4f470471aca458e35793f081f74f426a458591d
Paul Kramme <pjkramme@gmail.com>
2016-10-13 14:39:01 +0200

    Add working code! YAAAAAAAAAAS

--- client.py
+++ client.py
@@ -1,21 +1,49 @@
 #!/usr/bin/env python3
 import socket
 import sys
+import os
+from sendfile import sendfile
 #TCP_IP = '192.168.178.31' #FILL WITH IP WHICH REPRESENTS THE SERVER
-TCP_IP = sys.argv[1]
-TCP_PORT = 8000
-BUFFER_SIZE = 1024
-MESSAGE = bytes("start paint", 'utf-8')
 
+TCP_IP = sys.argv[2]
+TCP_PORT = 8000
+
+#BUFFER_SIZE = 1024
+#BUFFER = None
+
+path = sys.argv[1]
+print(path)
+file = open(path, "r")
+
+blocksize = os.path.getsize(path)
+sock = socket.socket()
+
+sock.connect((TCP_IP, TCP_PORT))
+offset = 0
+
+while 1:
+	sent = sendfile(sock.fileno(), file.fileno(), offset, blocksize)
+	if sent == 0:
+		print("Done.")
+		break
+	offset += sent
+
+
+
+
+
+
+
+"""
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
 	s.connect((TCP_IP, TCP_PORT))
 except:
 	print("Fail.")
 while 1:
-	s.send(bytes(input(), 'utf-8'))
+	s.send(BUFFER)
 	#data = s.recv(BUFFER_SIZE)
 s.close()
 
 #print("received data:", data)
-
+"""
--- server.py
+++ server.py
@@ -3,7 +3,9 @@ import socket
 
 TCP_IP = '' #FILL WITH IP WHICH REPRESENTS THE SERVER
 TCP_PORT = 8000
-BUFFER_SIZE = 1024
+#BUFFER_SIZE = 1024
+
+file = open("yay", "wb")
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
@@ -12,10 +14,12 @@ s.listen(1)
 conn, addr = s.accept()
 print('Connection address:', addr)
 while 1:
-	data = conn.recv(BUFFER_SIZE)
+	data = conn.recv(4096)
 	if not data:
+		print("Done")
 		break
-	print(str(data, 'utf-8'))
+	file.write(data)
 	#conn.send(data)
+file.close()
 conn.close()
 
